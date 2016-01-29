905732dd2517da229a83192f93bec4148d14b533
Seniority <senioritylabs@gmail.com>
2016-01-28 19:14:56 -0800

    first commit

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.gameoverLabel = new System.Windows.Forms.Label();
+            this.muteCheckbox = new System.Windows.Forms.CheckBox();
+            this.themeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.desertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jungleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.musicToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.onToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.offToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.NavajoWhite;
+            this.canvas.Image = ((System.Drawing.Image)(resources.GetObject("canvas.Image")));
+            this.canvas.Location = new System.Drawing.Point(12, 27);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(593, 485);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 515);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score:";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(73, 515);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(0, 17);
+            this.scoreLabel.TabIndex = 2;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(619, 24);
+            this.menuStrip.TabIndex = 3;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.themeToolStripMenuItem,
+            this.musicToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // gameoverLabel
+            // 
+            this.gameoverLabel.AutoSize = true;
+            this.gameoverLabel.BackColor = System.Drawing.Color.NavajoWhite;
+            this.gameoverLabel.Font = new System.Drawing.Font("Monotype Corsiva", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameoverLabel.Location = new System.Drawing.Point(128, 145);
+            this.gameoverLabel.Name = "gameoverLabel";
+            this.gameoverLabel.Size = new System.Drawing.Size(203, 43);
+            this.gameoverLabel.TabIndex = 4;
+            this.gameoverLabel.Text = "gameoverLabel";
+            this.gameoverLabel.Visible = false;
+            // 
+            // muteCheckbox
+            // 
+            this.muteCheckbox.AutoSize = true;
+            this.muteCheckbox.Enabled = false;
+            this.muteCheckbox.Location = new System.Drawing.Point(525, 516);
+            this.muteCheckbox.Name = "muteCheckbox";
+            this.muteCheckbox.Size = new System.Drawing.Size(81, 17);
+            this.muteCheckbox.TabIndex = 5;
+            this.muteCheckbox.Text = "Mute Music";
+            this.muteCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // themeToolStripMenuItem
+            // 
+            this.themeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.desertToolStripMenuItem,
+            this.jungleToolStripMenuItem});
+            this.themeToolStripMenuItem.Name = "themeToolStripMenuItem";
+            this.themeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.themeToolStripMenuItem.Text = "Theme";
+            // 
+            // desertToolStripMenuItem
+            // 
+            this.desertToolStripMenuItem.Name = "desertToolStripMenuItem";
+            this.desertToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.desertToolStripMenuItem.Text = "Desert";
+            // 
+            // jungleToolStripMenuItem
+            // 
+            this.jungleToolStripMenuItem.Name = "jungleToolStripMenuItem";
+            this.jungleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.jungleToolStripMenuItem.Text = "Jungle";
+            // 
+            // musicToolStripMenuItem
+            // 
+            this.musicToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.onToolStripMenuItem,
+            this.offToolStripMenuItem});
+            this.musicToolStripMenuItem.Name = "musicToolStripMenuItem";
+            this.musicToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.musicToolStripMenuItem.Text = "Music";
+            // 
+            // onToolStripMenuItem
+            // 
+            this.onToolStripMenuItem.Name = "onToolStripMenuItem";
+            this.onToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.onToolStripMenuItem.Text = "On";
+            // 
+            // offToolStripMenuItem
+            // 
+            this.offToolStripMenuItem.Name = "offToolStripMenuItem";
+            this.offToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.offToolStripMenuItem.Text = "Off";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 537);
+            this.Controls.Add(this.gameoverLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.menuStrip);
+            this.Controls.Add(this.muteCheckbox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label gameoverLabel;
+        private System.Windows.Forms.CheckBox muteCheckbox;
+        private System.Windows.Forms.ToolStripMenuItem themeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem desertToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jungleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem musicToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem onToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem offToolStripMenuItem;
+    }
+}
+
---
+++ Snake/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    /// <summary>
+    /// Snake by Anthony Senior (Seniority Labs™).
+    /// 1/12/2016
+    /// </summary>    
+    public partial class Form1 : Form
+    {
+        System.Media.SoundPlayer musicPlayer = new System.Media.SoundPlayer();
+
+        private List<Circle> Snake = new List<Circle>();
+        private Circle food = new Circle();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            musicPlayer.SoundLocation = "desertThemeMusic.wav";
+            musicPlayer.PlayLooping();
+
+            //Set settings to default
+            new Settings();
+
+            //Set game speed and start timer
+            gameTimer.Interval = 1000 / Settings.Speed;
+            gameTimer.Tick += UpdateScreen;
+            gameTimer.Start();
+
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            gameoverLabel.Visible = false;
+
+            //Set settings to default
+            new Settings();
+            Snake.Clear();
+
+            //Create new player object
+            Circle head = new Circle();
+            head.X = 25;
+            head.Y = 20;
+            Snake.Add(head);
+
+            scoreLabel.Text = Settings.Score.ToString();
+            GenerateFood();
+        }
+
+        //Place food randomly on canvas
+        private void GenerateFood()
+        {
+            int maxXPos = canvas.Size.Width / Settings.Width;
+            int maxYPos = canvas.Size.Height / Settings.Height;
+
+            Random random = new Random();
+            food = new Circle();
+            food.X = random.Next(0, maxXPos);
+            food.Y = random.Next(0, maxYPos);
+        }
+
+        private void UpdateScreen(object sender, EventArgs e)
+        {
+            //Check to see if player has lost
+            if (Settings.GameOver == true)
+            {
+                if (Input.KeyPressed(Keys.Enter))
+                {
+                    StartGame();
+                }
+            }
+            else
+            {
+                if (Input.KeyPressed(Keys.Right) && (Settings.direction != Direction.Left))
+                {
+                    Settings.direction = Direction.Right;
+                }
+                else if (Input.KeyPressed(Keys.Left) && (Settings.direction != Direction.Right))
+                {
+                    Settings.direction = Direction.Left;
+                }
+                else if (Input.KeyPressed(Keys.Up) && (Settings.direction != Direction.Down))
+                {
+                    Settings.direction = Direction.Up;
+                }
+                else if (Input.KeyPressed(Keys.Down) && (Settings.direction != Direction.Up))
+                {
+                    Settings.direction = Direction.Down;
+                }
+
+                MovePlayer();
+            }
+
+            canvas.Invalidate();
+        }
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics canvas = e.Graphics;
+
+            if (Settings.GameOver != true)
+            {
+                //Color of snake
+                Brush snakeColor;
+
+                //Draw snake
+                for (int i = 0; i < Snake.Count; i++)
+                {                    
+                    if (i == 0)
+                    {
+                        snakeColor = Brushes.Black; //head
+                    }
+                    else
+                    {
+                        snakeColor = Brushes.SaddleBrown; //body
+                    }
+
+                    //Snake design
+                    canvas.FillEllipse(snakeColor,
+                            new Rectangle(Snake[i].X * Settings.Width, Snake[i].Y * Settings.Height, Settings.Width, Settings.Height));
+                    //Food design
+                    canvas.FillEllipse(Brushes.Red,
+                        new Rectangle(food.X * Settings.Width, food.Y * Settings.Height, Settings.Width, Settings.Height));
+                }
+            }
+            else
+            {
+                string gameOver = "Game over!\n";
+                gameOver += "Your final score is: " + Settings.Score + "\n";
+                gameOver += "Press ENTER to try again";
+                gameoverLabel.Text = gameOver;
+                gameoverLabel.Visible = true;
+            }
+        }
+
+        private void MovePlayer()
+        {
+            for (int i = Snake.Count -1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    switch (Settings.direction)
+                    {
+                        case Direction.Right:
+                            Snake[i].X++;
+                            break;
+                        case Direction.Left:
+                            Snake[i].X--;
+                            break;
+                        case Direction.Up:
+                            Snake[i].Y--;
+                            break;
+                        case Direction.Down:
+                            Snake[i].Y++;
+                            break;
+                    }
+
+                    //Get max X and Y position
+                    int maxXPos = canvas.Size.Width / Settings.Width;
+                    int maxYPos = canvas.Size.Height / Settings.Height;
+
+                    //Detect collision with game "wall"
+                    if ((Snake[i].X < 0) || (Snake[i].Y < 0) || (Snake[i].X > maxXPos) || (Snake[i].Y > maxYPos))
+                    {
+                        Die();
+                    }
+
+                    //Detect collision with body
+                    for (int j = 1; j < Snake.Count; j++)
+                    {
+                        if ((Snake[i].X == Snake[j].X) && (Snake[i].Y == Snake[j].Y))
+                        {
+                            Die();
+                        }
+                    }
+
+                    //Detect collision with food
+                    if ((Snake[0].X == food.X) && (Snake[0].Y == food.Y))
+                    {
+                        Eat();
+                    }
+                }
+                else
+                {
+                    Snake[i].X = Snake[i - 1].X;
+                    Snake[i].Y = Snake[i - 1].Y;
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Input.ChangeState(e.KeyCode, true);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            Input.ChangeState(e.KeyCode, false);
+        }
+
+        private void Die()
+        {
+            Settings.GameOver = true;
+        }
+
+        private void Eat()
+        {
+            Circle food = new Circle();
+            food.X = Snake[Snake.Count - 1].X;
+            food.Y = Snake[Snake.Count - 1].Y;
+
+            Snake.Add(food);
+
+            //Update score
+            Settings.Score += Settings.Points;
+            scoreLabel.Text = Settings.Score.ToString();
+
+            GenerateFood();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string about_message = "";
+            about_message += "Welcome to the classic game of Snake by Seniority Labs™\n\n";
+            about_message += "In Snake, the your objective is to feed the snake by eating\n";
+            about_message += "food, which is represented by the stationary dot. In the beginning\n";
+            about_message += "of the game, your snake will be small, but as you eat, your\n";
+            about_message += "snake will become longer. Be careful to not hit the boundary\n";
+            about_message += "walls or you will die and the game will end. The game will also\n";
+            about_message += "end if your snake's head collides with it's own body. Good luck\n";
+            about_message += "and have fun!";
+
+            MessageBox.Show(about_message, "Snake", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }//end class
+}//end namespace
